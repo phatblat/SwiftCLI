1991935516340508c7055d32d87453e61fa956b4
Ben Chatelain <ben@octop.ad>
2020-11-05 07:35:29 -0700

    ⬆️ Quick (3.0.0), Nimble (9.0.0), ArgumentParser (0.3.1)

--- Package.swift
+++ Package.swift
@@ -19,9 +19,9 @@ let package = Package(
             targets: ["SwiftCLIKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "2.2.0")),
-        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "8.0.9")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "0.3.1")),
+        .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "9.0.0")),
     ],
     targets: [
         .target(
